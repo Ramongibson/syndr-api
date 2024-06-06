3ca00b452f1bc0ce755f8491ad59727f56e54c60
Ramongibson <ramongibson23@gmail.com>
2024-06-06 15:50:26 -0500

    initial commit

---
+++ src/main/java/com/ramon/gibson/syndrapi/SyndrApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.gibson.syndrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyndrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyndrApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ramon/gibson/syndrapi/SyndrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.gibson.syndrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyndrApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
